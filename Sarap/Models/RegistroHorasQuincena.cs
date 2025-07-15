a270393c5cc2a2dcdfc0aa00c20d0034d57fba32
Emmanuel <kchavarria20321@ufide.ac.cr>
2025-07-15 06:43:33 -0600

    Push

---
+++ Sarap/Models/RegistroHorasQuincena.cs
@@ -0,0 +1,18 @@
+﻿namespace Sarap.Models
+{
+    public class RegistroHorasQuincena
+    {
+        public int Id { get; set; }
+        public string Identidad { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public decimal HorasOrdinarias { get; set; }
+        public decimal HorasIncapacidad { get; set; }
+        public decimal HorasVacaciones { get; set; }
+        public decimal HorasFeriadoLey { get; set; }
+        public decimal HorasExtra15 { get; set; }
+        public decimal HorasExtra20 { get; set; }
+        public decimal HorasPermisoSinGoce { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
---
+++ Sarap/Models/VacacionesEmpleado.cs
@@ -0,0 +1,12 @@
+﻿namespace Sarap.Models
+{
+    public class VacacionesEmpleado
+    {
+        public int Id { get; set; }
+        public string Identidad { get; set; }
+        public string NombreCompleto { get; set; }
+        public int DiasDisponibles { get; set; }
+        public int DiasUsados { get; set; }
+        public DateTime? FechaUltimaActualizacion { get; set; }
+    }
+}

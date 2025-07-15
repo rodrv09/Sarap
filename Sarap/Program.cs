92eaa574466456b5ed4475142e45817661835b56
jefferRB <05jeffer03@gmail.com>
2025-07-15 13:19:37 -0600

    union con correcciones

--- Sarap/Program.cs
+++ Sarap/Program.cs
@@ -1,24 +1,31 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 using Repository;
+using Sarap.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+// Configuración de la conexión a la base de datos
 builder.Services.AddTransient<ClienteRepository>();
 builder.Services.AddTransient<ProveedorRepository>();
 builder.Services.AddTransient<UsuarioRepository>();
 builder.Services.AddTransient<EmpleadoRepository>();
+builder.Services.AddDbContext<EspeciasSarapiquiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 // Agrega autenticación por cookies
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
+    .AddCookie(options =>   
     {
         options.LoginPath = "/Account/Login";
         options.LogoutPath = "/Account/Logout";
         // Opcional: options.AccessDeniedPath = "/Account/AccessDenied";
     });
+// Configuración de la autenticación y autorización
 
 var app = builder.Build();
 
